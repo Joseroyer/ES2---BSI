9ef6ff395b94a42941ea84fb068fc0a5b3595a49
Lucas Isquerdo <lucasisquerdo22@gmail.com>
2022-11-02 11:52:56 -0300

    backend

---
+++ src/main/java/es2/projeto/librarytree/LibrarytreeApplication.java
@@ -0,0 +1,13 @@
+package es2.projeto.librarytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarytreeApplication.class, args);
+	}
+
+}
