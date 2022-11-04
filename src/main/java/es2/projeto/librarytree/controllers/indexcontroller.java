938754450024d58f4a7adb3b02b7388e8b8bd1d6
Lucas Isquerdo <lucasisquerdo22@gmail.com>
2022-11-04 12:57:13 -0300

    Home

--- src/main/java/es2/projeto/librarytree/controllers/indexcontroller.java
+++ src/main/java/es2/projeto/librarytree/controllers/indexcontroller.java
@@ -1,5 +1,26 @@
 package es2.projeto.librarytree.controllers;
 
-public class indexcontroller {
-        //teste
+import es2.projeto.librarytree.repositories.indexRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apis")
+public class indexcontroller 
+{
+//     @Autowired
+//     indexRepository indexRepository; 
+
+//     @RequestMapping("/parametrizacao")
+//     public ResponseEntity <Object> parameters(@RequestParam(value = "filtro")String filtro)
+//     {   
+        
+//     }
+
 }
