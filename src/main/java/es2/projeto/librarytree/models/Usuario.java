1545cf1bff61460135584688cc6b8bcd6761b4d5
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-05 14:03:36 -0300

    Update Usuario.java

--- src/main/java/es2/projeto/librarytree/models/Usuario.java
+++ src/main/java/es2/projeto/librarytree/models/Usuario.java
@@ -1,16 +1,93 @@
 package es2.projeto.librarytree.models;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import javax.persistence.*;
 
-@AllArgsConstructor //cria construtor
-@Data // gera get e set
+
+@Entity
+@Table(name = "Usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usu_id")
     private Long id;
+    @Column(name = "usu_nome")
     private String nome;
+    @Column(name = "usu_email")
     private String email;
+    @Column(name = "usu_senha")
     private String senha;
+    @Column(name = "usu_CPF")
     private String CPF;
+    @Column(name = "usu_telefone")
     private String telefone;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nome, String email, String senha, String CPF, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.CPF = CPF;
+        this.telefone = telefone;
+    }
+
+    public Usuario(Long id, String nome, String email, String senha, String CPF, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.CPF = CPF;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
