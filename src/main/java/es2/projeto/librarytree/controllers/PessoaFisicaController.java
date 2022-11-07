0a936a8e8f0fd8ed51514f9647c198e6e65c89a3
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-06 21:43:23 -0300

    delete Usuario

--- src/main/java/es2/projeto/librarytree/controllers/PessoaFisicaController.java
+++ src/main/java/es2/projeto/librarytree/controllers/PessoaFisicaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/apis")
+@RequestMapping("/apis")
 @RequiredArgsConstructor
 public class PessoaFisicaController {
     private final PessoaFisicaService pessoaFisicaService;
