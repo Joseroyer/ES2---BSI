c4496730ca842dd281eb5ba3a91e4cdd87443ef7
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-11 19:07:48 -0300

    import model bibliotecario

--- src/main/java/es2/projeto/librarytree/models/Bibliotecario.java
+++ src/main/java/es2/projeto/librarytree/models/Bibliotecario.java
@@ -1,109 +1,104 @@
 package es2.projeto.librarytree.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @Entity
 @Table(name = "bibliotecario")
 public class Bibliotecario {
+
     @Id
-    @Column(name = "bi_id_b")
+    @Column(name = "bi_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne
-    @JoinColumn(name = "pessoafisica_cpf", nullable = false)
-    private PessoaFisica pessoafisica_fk;
 
     @Column(name = "bi_dta_admissao")
     private Date data_admissao;
 
     @Column(name = "bi_dta_demissao")
     private Date data_demissao;
+
     @Column(name = "bi_login")
     private String login;
+
     @Column(name = "bi_senha")
     private String senha;
+
     @Column(name = "bi_status")
     private int status;
 
+    @Column(name = "bi_nivel")
+    private int nivel;
+
+    @Column(name = "bi_nome")
+    private String nome;
+
+    @Column(name = "bi_cpf")
+    private String CPF;
+
+    @Column(name = "bi_telefone")
+    private String telefone;
+
+    @Column(name = "bi_email")
+    private String email;
+
     public Bibliotecario() {
+
     }
 
-    public Bibliotecario(PessoaFisica pessoafisica_fk, Date data_admissao, Date data_demissao, String login,
-            String senha, int status) {
-        this.pessoafisica_fk = pessoafisica_fk;
+
+    public Bibliotecario(Date data_admissao, Date data_demissao, String login, String senha, int status, int nivel,
+                         String nome, String cPF, String telefone, String email) {
         this.data_admissao = data_admissao;
         this.data_demissao = data_demissao;
         this.login = login;
         this.senha = senha;
         this.status = status;
+        this.nivel = nivel;
+        this.nome = nome;
+        CPF = cPF;
+        this.telefone = telefone;
+        this.email = email;
     }
 
-    public Bibliotecario(Long id, PessoaFisica pessoafisica_fk, Date data_admissao, Date data_demissao, String login,
-            String senha, int status) {
+
+    public Bibliotecario(Long id, Date data_admissao, Date data_demissao, String login, String senha, int status,
+                         int nivel, String nome, String cPF, String telefone, String email) {
         this.id = id;
-        this.pessoafisica_fk = pessoafisica_fk;
         this.data_admissao = data_admissao;
-        this.data_demissao = data_demissao;
+        this.data_demissao = null;
         this.login = login;
         this.senha = senha;
         this.status = status;
+        this.nivel = nivel;
+        this.nome = nome;
+        CPF = cPF;
+        this.telefone = telefone;
+        this.email = email;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public PessoaFisica getPessoafisica_fk() {
-        return pessoafisica_fk;
-    }
-
-    public void setPessoafisica_fk(PessoaFisica pessoafisica_fk) {
-        this.pessoafisica_fk = pessoafisica_fk;
-    }
-
-    public Date getData_admissao() {
-        return data_admissao;
-    }
-
-    public void setData_admissao(Date data_admissao) {
+    public Bibliotecario(Date data_admissao, String login, String senha, int status, int nivel, String nome, String CPF, String telefone, String email) {
         this.data_admissao = data_admissao;
-    }
-
-    public Date getData_demissao() {
-        return data_demissao;
-    }
-
-    public void setData_demissao(Date data_demissao) {
-        this.data_demissao = data_demissao;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
         this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
         this.senha = senha;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
         this.status = status;
+        this.nivel = nivel;
+        this.nome = nome;
+        this.CPF = CPF;
+        this.telefone = telefone;
+        this.email = email;
     }
+    //    private  static  BibliotecarioRepository bibliotecarioRepository;
+//
+//    public Bibliotecario save(Bibliotecario bibliotecario){
+//        return bibliotecarioRepository.save(bibliotecario);
+//    }
+//
+//    public List <Bibliotecario> findFilterWith(String senha,String login){
+//        return bibliotecarioRepository.findFilterWith(senha, login);
+//    }
+
 }
