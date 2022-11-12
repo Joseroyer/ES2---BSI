b91d7e224475279270080b7d4fc6fc7a8622724a
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-12 11:10:43 -0300

    Update LoginControl.java

--- src/main/java/es2/projeto/librarytree/control/LoginControl.java
+++ src/main/java/es2/projeto/librarytree/control/LoginControl.java
@@ -41,24 +41,23 @@ public class LoginControl {
     @PostMapping("/autenticar")
     public ResponseEntity<Object> autenticar1(String login, String senha, boolean adm) {
         int i = 0, pos = 0;
+        String token = "";
         if (adm == true) {
             List<Bibliotecario> user = bibliotecarioRepository.findFilterWith(senha, login);
             for (i = 0; i < user.size(); i++)
-                if (senha.equals(user.get(i).getSenha()) && login.equals(user.get(i).getLogin()))
+                if (senha.equals(user.get(i).getSenha()) && login.equals(user.get(i).getLogin()) && user.get(i).getStatus() == 1)
                     pos = 1;
-            String token = "";
             if (pos == 1) {
                 token = JWTTokenProvider.getToken(senha, "ADM");
                 System.out.println(token);
                 return new ResponseEntity<>(token, HttpStatus.OK);
             } else
-                return new ResponseEntity<>("ACESSO NAO PERMITIDO", HttpStatus.NOT_ACCEPTABLE);
+                return new ResponseEntity<>("Não ativo", HttpStatus.NOT_ACCEPTABLE);
         } else {
             List<Cliente> user = clienteRepository.findAllWithFilter(senha, login);
             for (i = 0; i < user.size(); i++)
                 if (senha.equals(user.get(i).getSenha()) && login.equals(user.get(i).getLogin()))
                     pos = 1;
-            String token = "";
             if (pos == 1) {
                 token = JWTTokenProvider.getToken(senha, "ADM");
                 System.out.println(token);
