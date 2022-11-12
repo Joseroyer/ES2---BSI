1247118bea2b528b187142490bc2398958d8967a
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-11 22:31:36 -0300

    update

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -163,7 +163,7 @@ function verifica() {
 
 //Nome, Email, nome, Senha
 function gravarUsuario() {
-    const URL = "/security/save";
+    const URL = "/apis/saveCliente";
     var fdados = document.getElementById("fdados");
     var jsontext = JSON.stringify(Object.fromEntries(
         new FormData(fdados)));
