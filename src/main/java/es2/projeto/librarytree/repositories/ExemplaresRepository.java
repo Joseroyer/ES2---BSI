5b9730a5e5bf9eda695c75ee333ee5871ad37a48
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-21 15:51:12 -0300

    Query List Exemplares

--- src/main/java/es2/projeto/librarytree/repositories/ExemplaresRepository.java
+++ src/main/java/es2/projeto/librarytree/repositories/ExemplaresRepository.java
@@ -1,7 +1,14 @@
 package es2.projeto.librarytree.repositories;
 
-import es2.projeto.librarytree.models.Exemplares;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import es2.projeto.librarytree.models.Exemplares;
 
 public interface ExemplaresRepository extends JpaRepository<Exemplares, Long> {
+
+    @Query(value = "SELECT * FROM exemplares WHERE status = 1 ORDER BY id_exemplar ASC", nativeQuery = true)
+    public List<Exemplares> findAllExemplar();
 }
--- src/main/java/es2/projeto/librarytree/singleton/SingletonExemplares.java
+++ src/main/java/es2/projeto/librarytree/singleton/SingletonExemplares.java
@@ -1,12 +1,13 @@
 package es2.projeto.librarytree.singleton;
 
-import es2.projeto.librarytree.models.Exemplares;
-import es2.projeto.librarytree.repositories.ExemplaresRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import es2.projeto.librarytree.models.Exemplares;
+import es2.projeto.librarytree.repositories.ExemplaresRepository;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
@@ -15,12 +16,11 @@ public class SingletonExemplares {
     private final ExemplaresRepository exemplaresRepository;
 
     public List<Exemplares> findAllExemplar() {
-        return exemplaresRepository.findAll();
+        return exemplaresRepository.findAllExemplar();
     }
 
     public Exemplares saveExemplar(Exemplares exemplares) {
         return exemplaresRepository.save(exemplares);
     }
-
 
 }
