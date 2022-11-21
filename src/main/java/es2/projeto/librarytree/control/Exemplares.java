e07fd984d413838a97f375d8e63dd17f3c1c5bc0
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2022-11-21 15:39:50 -0300

    Update Registrar Exemplar
    listando os exemplares

--- src/main/java/es2/projeto/librarytree/control/Exemplares.java
+++ src/main/java/es2/projeto/librarytree/control/Exemplares.java
@@ -1,11 +1,16 @@
 package es2.projeto.librarytree.control;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import es2.projeto.librarytree.singleton.SingletonExemplares;
 import es2.projeto.librarytree.singleton.SingletonLivro;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,7 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class Exemplares {
     private final SingletonExemplares singletonExemplares;
     private final SingletonLivro singletonLivro;
-    @GetMapping("listar_exemplares")
+
+    @RequestMapping("listar_exemplares")
     public ResponseEntity<Object> listarExemplar() {
         return new ResponseEntity<>(singletonExemplares.findAllExemplar(), HttpStatus.OK);
     }
--- src/main/resources/static/js/RegistrarExemplar.js
+++ src/main/resources/static/js/RegistrarExemplar.js
@@ -1,12 +1,12 @@
 function CarregaLista() {
-    const URL_TO_FETCH = '/apis/listaradm';
+    const URL_TO_FETCH = '/apis/listar_exemplares';
     fetch(URL_TO_FETCH, {
-        method: 'POST',
+        method: 'Post',
         headers: { 'Authorization': `${localStorage.getItem("token")}`, }
     })
         .then(response => response.text())
         .then(result => {
-            fetch("/apis/listaradm")
+            fetch("/apis/listar_exemplares")
                 .then(function (response) {
                     return response.json();
                 })
@@ -17,22 +17,42 @@ function CarregaLista() {
                     console.log('error: ' + err);
                 });
             function appendData(data) {
-
+                console.log(data);
                 var table = "";
+                table += `<tr><th>#</th><th>Livro</th><th>Editora</th><th>Quantidade</th><th>Ano Publicado</th><th>Editar</th><th>Excluir</th></tr>`
                 for (let i = 0; i < data.length; i++) {
                     table += `<tr>
                             <td>${data[i].id}</td>
-                            <td>${data[i].nome}</td>
-                            <td>${data[i].telefone}</td>
-                            <td>${data[i].email}</td>                            
+                            <td>${data[i].livro_fk.titulo}</td>
+                            <td>${data[i].editora_fk.nome_editora}</td>
+                            <td>${data[i].qtd}</td> 
+                            <td>${data[i].ano_publicado}</td>                            
                             <td><img width="30px" src='img/change.png' onclick='editar(${data[i].id})'></td>
                             <td><img width="30px" src='img/trash.png' onclick='excluir(${data[i].id})'></td>
                             </tr>`;
                 }
-                document.getElementById("thead").style.display = "block";
                 document.getElementById("tbody").innerHTML = table;
             }
-        }
-        )
+        })
         .catch(err => console.error(err));
 }
+// function exibirCat() {
+//     fetch("/api/listar-todos")
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             appendData(data);
+//         })
+//         .catch(function (err) {
+//             console.log('error: ' + err);
+//         });
+//     function appendData(data) {
+
+//         var resp = "";
+//         for (let i = 0; i < data.length; i++)
+//             resp += `<option value="` + (i + 1) + `">` + `${data[i].nome}</option>`;
+//         document.getElementById("categoria").innerHTML = resp;
+//     }
+
+// }
